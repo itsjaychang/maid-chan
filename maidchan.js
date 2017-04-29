3469f6fb7f1c98ee64d84c797bdd7520f4b3cacd
Jay Chang <jaychanggg@gmail.com>
2017-04-28 17:54:50 -0700

    Added pokemon feature + refactor promises
    Refactored gamble into promises

--- maidchan.js
+++ maidchan.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const {stripIndents} = require('common-tags');
+// const {stripIndents} = require('common-tags');
 const mongoose = require('mongoose');
 // const path = require('path');
 const unirest = require('unirest');
@@ -12,7 +12,7 @@ const user = require('./extra/user.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-	mongoose.connect(config.mongoLabURL);
+	mongoose.connect(config.mongoURI);
 	database = mongoose.model('user');
 	console.log('Maid-Chan is ready to serve you Master~~~~');
 });
@@ -24,6 +24,44 @@ client.on('message', (message) => {
 	if (message.channel.type == 'dm') return;
 	if (message.author.bot) return;
 	if (!msg.startsWith(prefix)) return;
+
+	// pokemon
+	if (msg.startsWith(`${prefix} pokemon`)) {
+		const getPokemonCount = function() {
+			const promise = new Promise((resolve, reject) => {
+				unirest.get('http://pokeapi.co/api/v2/pokemon-species')
+					.end(function(res) {
+						resolve(res.body.count);
+					});
+			}); 
+			return promise;
+		}
+		const getPokemon = function(count) {
+			const randomId = Math.floor(Math.random()*(count-1)+1);
+			const promise = new Promise((resolve, reject) => {
+				unirest.get('http://pokeapi.co/api/v2/pokemon/'+randomId)
+					.end(function(res) {
+						resolve(res.body);
+					});
+			})
+			return promise;
+		}
+		const parsePokemonJSON = function(pokemonJSON) {
+			console.log('works');
+			const name = pokemonJSON.name.replace(/(^|\s)[a-z]/g,function(f){return f.toUpperCase()});;
+			const spriteURL = pokemonJSON.sprites.front_default;
+			const embed = new Discord.RichEmbed()
+  				.setDescription(`A wild **${name}** appeared!!`)	
+  				.setImage(spriteURL)
+  			message.channel.sendEmbed(embed);
+		}
+		
+		getPokemonCount()
+			.then(getPokemon)
+			.then(parsePokemonJSON);
+
+		return;
+	}
 
 	// Slots
 	if (msg.startsWith(`${prefix} credits`)) {
@@ -43,41 +81,64 @@ client.on('message', (message) => {
 	}
 
 	if (msg.startsWith(`${prefix} gamble`)) {
+		var splits = message.content.split(' ');
+		var gambleAmount = Number(splits[2]);
 		const id = message.author.id;
-		database.findOne({'userId': id}, function(err,user) {
+
+		if (Number.isNaN(gambleAmount)) {
+			message.channel.sendMessage('Please gamble with proper amount baka');
+			return;
+		}
+
+		const getUser = function() {
+			const promise = new Promise((resolve, reject) => {
+				database.findOne({'userId': id}, function(err, user) {
+					resolve(user);
+				})
+			});
+			return promise;
+		}
+
+		const gamble = function(user) {
+			const promise = new Promise((resolve, reject) => {
+				const currentAmount = user.points;
+				const win = Math.random() < 0.5 ? true : false;
+				const newAmount = win ? (currentAmount + gambleAmount) : (currentAmount - gambleAmount);
+				message.channel.sendMessage(`**${message.author.username}**-sama you ${win ? 'win' : 'lose'}, ${currentAmount} => ${newAmount}`)
+				resolve(newAmount);
+			});
+			return promise;
+		}
+
+		const updateUser = function (newAmount) {
+			console.log(newAmount);
+			database.update({'userId': id}, {$set: {'points': newAmount}}, function (err, user) {
+				if (err) return handleError(err);
+			});
+			return;
+		}
+
+		const conditional = function(user) {
 			if (user == null) {
 				message.channel.sendMessage(`**${message.author.username}**-sama, do credits first <3`);
 				return;
 			} else if (user.points <= 0) {
-				message.channel.sendMessage(`**${message.author.username}**-sama, your such a peasant, you have ${user.points}`);
+				message.channel.sendMessage(`**${message.author.username}**-sama, your such a peasant, you have ${user.points} points :,(`);
 				return;
 			} else {
-				var splits = message.content.split(' ');
-				var gamble = Number(splits[2]);
-				if (gamble == NaN) {
-					message.channel.sendMessage('Please gamble with proper amount baka');
-					return;
-				}
-				var currentAmount = user.points;
-				if (Math.random() > 0.5) {
-					newAmount = currentAmount - gamble;
-					message.channel.sendMessage(`**${message.author.username}**-sama you lose, ${currentAmount} => ${newAmount}`);
-				} else {
-					newAmount = currentAmount + gamble;
-					message.channel.sendMessage(`**${message.author.username}**-sama you won, ${currentAmount} => ${newAmount}`);
-				}
-				database.update({'userId': id}, {$set: {'points': newAmount}}, function (err, user) {
-                	if (err) return handleError(err);
-                });
+				return gamble(user).then(updateUser);
 			}
-		})
+		}
+		getUser()
+			.then(conditional);
+	
 		return;
 	}
 
 	// Both Masters and Baka
 	if (msg.startsWith(`${prefix} help`)) {
-		message.channel.sendEmbed({
-			description: `
+		message.channel.sendEmbed({ description: 
+			`
 				**Commands**
 
 				pocky
@@ -86,6 +147,7 @@ client.on('message', (message) => {
 				define _[~]_
 				credits
 				gamble [#]
+				pokemon
 			`
 		});
 		return;
@@ -126,10 +188,10 @@ client.on('message', (message) => {
 	if (msg.startsWith(`${prefix} define`)) {
 		var splits = message.content.split(' ');
 		splits.splice(0, 2);
-		const search = splits.join("+");
-		const word = splits.join(" ");
+		const search = splits.join('+');
+		const word = splits.join(' ');
 
-		unirest.get("http://api.urbandictionary.com/v0/define?term="+search)
+		unirest.get('http://api.urbandictionary.com/v0/define?term='+search)
 			.end(function(res) {
     			if (res.error) {
     				message.channel.sendMessage('Something bad happenened... T______T');
@@ -138,8 +200,8 @@ client.on('message', (message) => {
     					const item = res.body.list[0];
     					const definition = item.definition ? `${item.definition}` : '';
     					const example = item.example ? `${item.example}` : '';
-    					message.channel.sendEmbed({
-							description: `
+    					message.channel.sendEmbed({ description: 
+    						`
 								**${word}**
 
 								_Definition_
@@ -154,6 +216,11 @@ client.on('message', (message) => {
 					};
     			};
     		});
+		return;
+	}
+
+	if (msg == prefix) {
+		message.channel.sendMessage(`How may I help you today **${message.author.username}**-sama~~~`);
 		return;
 	}
 
